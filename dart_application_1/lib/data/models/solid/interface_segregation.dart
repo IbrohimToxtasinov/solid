9f7a44782e45a67260efd451d231bb5e25309170
IbrohimToxtasinov <toxtasinovibrohim976@gmail.com>
2023-02-16 17:37:09 +0500

    I - Interface Segregation

--- dart_application_1/lib/data/models/solid/interface_segregation.dart
+++ dart_application_1/lib/data/models/solid/interface_segregation.dart
@@ -1,18 +1,18 @@
 //I - Interface Segregation
 
-    abstract class Worker {
-    void work();
+abstract class Worker {
+  void work();
 }
 
 abstract class Sleeper {
-    void sleep();
+  void sleep();
 }
 
 class Human implements Worker, Sleeper {
-    void work() => print("I do a lot of work");
-    void sleep() => print("I need 10 hours per night...");
+  void work() => print("I do a lot of work");
+  void sleep() => print("I need 10 hours per night...");
 }
 
 class Robot implements Worker {
-    void work() => print("I always work");
+  void work() => print("I always work");
 }
