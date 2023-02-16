97f317876fcb089e95041e56fa75da60dc0e1ab9
IbrohimToxtasinov <toxtasinovibrohim976@gmail.com>
2023-02-16 17:03:54 +0500

    S - Single Responsibility

---
+++ dart_application_1/lib/data/models/solid/single_responsibility.dart
@@ -0,0 +1,19 @@
+//S — Single Responsibility
+
+class Calculator {
+  num add(num a, num b) {
+    return a + b;
+  }
+
+  num subtraction(num a, num b) {
+    return a - b;
+  }
+
+  num multiply(num a, num b) {
+    return a * b;
+  }
+
+  num divide(num a, num b) {
+    return a - b;
+  }
+}
