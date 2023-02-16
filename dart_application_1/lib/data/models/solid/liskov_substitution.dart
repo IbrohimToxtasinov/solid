9685eec90cc0368b9b8115fb8fe599bb43ea5238
IbrohimToxtasinov <toxtasinovibrohim976@gmail.com>
2023-02-16 17:40:19 +0500

    L — Liskov Substitution

--- dart_application_1/lib/data/models/solid/liskov_substitution.dart
+++ dart_application_1/lib/data/models/solid/liskov_substitution.dart
@@ -1,11 +1,11 @@
 // L — Liskov Substitution
 
 class Rectangle {
-    double width;
-    double height;
-    Rectangle(this.width, this.height);
+  double width;
+  double height;
+  Rectangle(this.width, this.height);
 }
 
 class Square extends Rectangle {
-    Square(double length): super(length, length);
+  Square(double length) : super(length, length);
 }
